717b3b12d5042978083132f3d20735f9b6b0393b
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-07 09:47:34 -0500

    modificando gitig, creando apps y aniadiendo un modelo a blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# los modelos se convierten en tablas
+class Proyecto (models.Model):
+    #columnas y tipos de datos
+    nombre = models.CharField(max_length=200)
+    caracteristica = models.CharField(max_length=300)
