0844b06ee5bed0d64253646037d9a5be6be704b7
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-08 09:07:39 -0500

    aniadiendo un proxy de persona

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,19 @@ class Proyecto (models.Model):
     caracteristica = models.CharField(max_length=300)
 
     def __str__(self):
-        return self.nombre+' -> '+self.caracteristica
+        return self.nombre +' -> '+ self.caracteristica
 
 class Tarea (models.Model):
     nombre = models.CharField(max_length=100)
     proyecto = models.ForeignKey(Proyecto,on_delete=models.CASCADE) #llave foranea con borrado forma cascada
     def __str__(self):
-        return self.nombre +' -> Proyecto: ' + self.proyecto.nombre
+        return self.nombre +' -> Proyecto: ' + self.proyecto
+
+#CREANDO UN PROXY
+class Persona(models.Model):
+    Primer_nombre = models.CharField(max_length=30)
+    Segundo_nombre = models.CharField(max_length=30)
+
+class PersonaProxy(Persona):
+    class Meta():
+        proxy = True
